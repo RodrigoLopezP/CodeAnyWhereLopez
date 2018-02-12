c88fe16e06338462c9be2b8cfa7b32e24600e0b8
rodrigolopezp98 <rodrigolopezp98@gmail.com>
2018-02-12 04:01:36 -0500

    .

--- Es_ModuloRegistrazione/Registrato.php
+++ Es_ModuloRegistrazione/Registrato.php
@@ -1,35 +1,59 @@
-<html>
-  <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<?php
+$nome=$_REQUEST["nome"];
+$cognome=$_REQUEST["cognome"];
+$sesso=$_REQUEST["sesso"];
+$nazionalita=$_REQUEST["nazionalita"];
+$email=$_REQUEST["email"];
+$patente=$_REQUEST["patente"];
 
-  </head>
-  <body>
-    <div class="row">
-      <div class="col-md-4">
-        
-      </div>
-           <div class="col-md-4">
-            <div class="card" style="background-color:gold;" >
-      <div class="card-header">
+$conn="mysql: host=localhost;dbname=miodb_Lopez";
+$pass="481516";
+$user="root";
+
+try {
+    $dbh=new PDO($conn,$user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+?>
+<html>
+
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <div class="row">
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="background-color:gold;">
+        <div class="card-header">
           <h1>
-      Esito registrazione
-    </h1>
-      </div>
-      <div class="card-body">
-           <h4 align="center">
-      Dati correttamente registrati
-    </h4>
-      </div>
-              <div class="card-footer" align="center">
-                <button class="btn">
+            Esito registrazione
+            <?php echo $nome?>
+          </h1>
+        </div>
+        <div class="card-body">
+          <h4 align="center">
+            Dati correttamente registrati
+          </h4>
+        </div>
+        <div class="card-footer" align="center">
+          <button class="btn">
                   Chiudi
                 </button>
-              </div>
-    </div>
-  
- 
+        </div>
       </div>
-    </div>
 
-  </body>
+
+    </div>
+  </div>
+
+</body>
+
 </html>
--- Es_ModuloRegistrazione/conferma.php
+++ Es_ModuloRegistrazione/conferma.php
@@ -24,34 +24,62 @@ else{
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
 
-  <body >
+  <body>
     <div class="row">
       <div class="col-md-4">
       </div>
-      <div class="col-md-4 " align="center" >
+      <div class="col-md-4 " align="center">
 
-
-
-        <form action="Registrato.php" method="post" >
+        <form action="Registrato.php" method="post">
           <div class="card" style="background-color:slategray; color:white;">
             <div class="card-header">
               <h1>Riepilogo dati </h1>
             </div>
             <div class="card-body">
-              <ul class="list-unstyled">
-                <li>Cognome: 
-                  <?php echo $cognome ?>
-                </li>
-                <li>Nome:
-                  <?php echo $nome ?> </li>
-                <li>Sesso:
-                  <?php echo $sesso ?> </li>
-                <li>Patente:
-                  <?php echo $patente ?> </li>
-                <li>Email:
-                  <?php echo $email ?> </li>
-              </ul>
-
+              <table>
+                <tr>
+                  <td>Cognome:</td>
+                  <td>
+                    <?php echo $cognome ?>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Nome:</td>
+                  <td>
+                    <?php echo $nome ?>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Nazionalita:</td>
+                  <td>
+                    <?php echo $nazionalita ?>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Sesso:</td>
+                  <td>
+                    <?php echo $sesso ?>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Patente:</td>
+                  <td>
+                    <?php echo $patente ?>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Email:</td>
+                  <td>
+                    <?php echo $email ?>
+                  </td>
+                </tr>
+              </table>
+              <input type="hidden" name="cognome" value="<?php echo $cognome ?>">
+              <input type="hidden" name="nome" value="<?php echo $nome ?>">
+              <input type="hidden" name="sesso" value="<?php echo $sesso ?>">
+              <input type="hidden" name="nazionalita" value="<?php echo $nazionalita ?>">
+              <input type="hidden" name="patente" value="<?php echo $patente ?>">
+              <input type="hidden" name="email" value="<?php echo $email ?>">
             </div>
             <div class="card-footer">
               <form action="index.php" method="post">
@@ -60,8 +88,8 @@ else{
               <button type="submit" class="btn btn-success ">        Registra        </button>
             </div>
           </div>
-
         </form>
+
       </div>
     </div>
 
